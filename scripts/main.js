9d71e6c48e640115afb9cf000844463d1727f22f
allucinatio <allucinatio@gmail.com>
2017-06-13 13:45:02 -0400

    Remove debugging console.logs and clean up code

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,10 @@
 // establish my hand as an array
 var myHand = ['7', 'J', '5'];
 
-
-
 function handValue (hand) {
 var hasAce = 0;
-console.log("--------------");
-console.log("hand initial: ", hand);
+// console.log("--------------");
+// console.log("hand initial: ", hand);
 
   // convert card strings to integers, except Aces
   for (var i = 0; i < hand.length; i++) {
@@ -26,32 +24,30 @@ console.log("hand initial: ", hand);
     } else if (hand[i] === 'A'){
       hasAce += 1;
       hand[i] = 0;
-
-      console.log("was ace removed? ", hand)
+      // console.log("was ace removed? ", hand)
     } else {
       hand[i] = parseInt(hand[i]);
     }
   }
 
   // add hand together
-  console.log("do I have an Ace?", hasAce)
-  console.log("hand after conversion: ", hand);
+  // console.log("do I have an Ace?", hasAce)
+  // console.log("hand after conversion: ", hand);
   hand = hand.reduce((a, b) => a + b, 0);
-  console.log("hand after sum: ", hand);
+  // console.log("hand after sum: ", hand);
 
   // now, deal with Aces being 1 or 11 now that we know the hand total
   while (hasAce >= 1) {
     if (hand <= 10) {
       hand = hand + 11;
-      console.log("Made Ace an 11");
+      // console.log("Made Ace an 11");
       hasAce--;
     } else {
       hand = hand + 1;
-      console.log("Made Ace a 1");
+      // console.log("Made Ace a 1");
       hasAce--;
     }
   }
-
 
   return hand;
 }
